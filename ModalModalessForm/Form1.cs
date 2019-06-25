871f56c1c8065356fee2a97d9cdc50b500c39301
Hajin Kim <enro302@naver.com>
2019-06-25 15:08:36 +0900

    Modal & Modaless 폼 디자인

--- ModalModalessForm/Form1.cs
+++ ModalModalessForm/Form1.cs
@@ -15,5 +15,27 @@ namespace ModalModalessForm
         {
             InitializeComponent();
         }
+
+        private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.statusMessage.Text = "종료합니다.";
+            Application.ExitThread();  // 어플리케이션 쓰레드 완전 종료
+
+        }
+
+        private void ModalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowModal();
+
+        }
+
+        private void ShowModal()
+        {
+            this.statusMessage.Text = "모달 창을 띄웁니다.";
+            FormModal m = new FormModal();
+            m.Title = "모달 창띄우기";
+            m.ShowDialog();
+        }
+
     }
 }
